5da8af1a18ce441aaf79663e9253c9aff464570a
Dana Arroway <dana.arroway@gmail.com>
2022-09-01 14:59:47 -0500

    fixed route redirect on login screen

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 
  exports.getLogin = (req, res) => {
     if (req.user) {
-      return res.redirect('/herbs')
+      return res.redirect('/')
     }
     res.render('login', {
       user: req.user, title: 'Login'
@@ -31,7 +31,7 @@ const User = require('../models/User')
       req.logIn(user, (err) => {
         if (err) { return next(err) }
         req.flash('success', { msg: 'Success! You are logged in.' })
-        res.redirect(req.session.returnTo || '/herbs')
+        res.redirect(req.session.returnTo || '/')
       })
     })(req, res, next)
   }
@@ -49,7 +49,7 @@ const User = require('../models/User')
   
   exports.getSignup = (req, res) => {
     if (req.user) {
-      return res.redirect('/herbs', { user: req.user })
+      return res.redirect('/', { user: req.user })
     }
     res.render('signup', {
       user: req.user, title: 'Create Account'
@@ -89,7 +89,7 @@ const User = require('../models/User')
           if (err) {
             return next(err)
           }
-          res.redirect('/herbs')
+          res.redirect('/')
         })
       })
     })
